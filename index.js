e48029d738e2efe6dab514c303fe396aac4ed029
ssaczkowski <sabrina.sacz@gmail.com>
2022-05-08 17:51:23 -0300

    background and sound buttons added

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-
-
 var drumButtons = document.querySelectorAll(".drum")
 
 for (var i = 0; i < drumButtons.length; i++) {
     drumButtons[i].addEventListener("click", function (){
-        
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "w":     
+                var audioButton = document.querySelector("audio#tom-1").play();
+                break;
+            case "a":
+                var audioButton = document.querySelector("audio#tom-2").play();
+                break;
+            case "s":
+                var audioButton = document.querySelector("audio#tom-3").play();
+                break;
+            case "d":
+                var audioButton = document.querySelector("audio#tom-4").play();
+                break;
+            case "j":
+                var audioButton = document.querySelector("audio#snare").play();
+                break;
+            case "k":
+                var audioButton = document.querySelector("audio#crash").play();
+                break;
+            case "l": 
+                var audioButton = document.querySelector("audio#kick-bass").play();
+                break;
+            default:
+                console.log(buttonInnerHTML);
+                break;
+        }
+    
     });
 }
 
